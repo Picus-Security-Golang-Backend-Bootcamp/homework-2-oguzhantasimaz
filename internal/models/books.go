08b37a918a22666a8245e31747662599f292c98c
Oguzhan Tasimaz <oguzhantasimaz@Oguzhans-MBP.home>
2022-03-06 12:46:02 +0300

    Add: isDeleted controls to Book struct, now isDeleted controls both in Library and Book struct

--- internal/models/books.go
+++ internal/models/books.go
@@ -1,6 +1,9 @@
 package models
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 //Book struct
 type Book struct {
@@ -22,28 +25,46 @@ func NewBook(id int, title string, stockCode string, stockCount int, isbn int, p
 
 //Get author name of the book
 func (b *Book) AuthorName() string {
-	return b.author.name + " " + b.author.surname
+	if !b.isDeleted {
+		return b.author.name + " " + b.author.surname
+	} else {
+		fmt.Println("You can't get author name of a deleted book")
+		return "Deleted book"
+	}
 }
 
 //Set stock count of the book
 func (b *Book) SetStockCount(count int) {
-	b.stockCount = count
+	if !b.isDeleted {
+		b.stockCount = count
+	} else {
+		fmt.Println("You can't set stock count of a deleted book")
+	}
 }
 
 //Set isDeleted of the book
 func (b *Book) SetIsDeleted(isDeleted bool) {
-	b.isDeleted = isDeleted
+	if !b.isDeleted {
+		b.isDeleted = isDeleted
+	} else {
+		fmt.Println("You can't set isDeleted of a deleted book")
+	}
 }
 
 //Return all information of the book as a string
 func (b *Book) GetBookInformation() string {
-	return "ID: " + strconv.Itoa(b.id) + "\n" +
-		"Title: " + b.title + "\n" +
-		"Stock Code: " + b.stockCode + "\n" +
-		"Stock Count: " + strconv.Itoa(b.stockCount) + "\n" +
-		"ISBN: " + strconv.Itoa(b.isbn) + "\n" +
-		"Page Count: " + strconv.Itoa(b.pageCount) + "\n" +
-		"Price: " + strconv.FormatFloat(b.price, 'f', 2, 64) + "\n" +
-		"Author: " + b.AuthorName() + "\n" +
-		"Is Deleted: " + strconv.FormatBool(b.isDeleted)
+	if !b.isDeleted {
+		return "ID: " + strconv.Itoa(b.id) + "\n" +
+			"Title: " + b.title + "\n" +
+			"Stock Code: " + b.stockCode + "\n" +
+			"Stock Count: " + strconv.Itoa(b.stockCount) + "\n" +
+			"ISBN: " + strconv.Itoa(b.isbn) + "\n" +
+			"Page Count: " + strconv.Itoa(b.pageCount) + "\n" +
+			"Price: " + strconv.FormatFloat(b.price, 'f', 2, 64) + "\n" +
+			"Author: " + b.AuthorName() + "\n" +
+			"Is Deleted: " + strconv.FormatBool(b.isDeleted)
+	} else {
+		fmt.Println("You can't get information of a deleted book")
+		return "Deleted book"
+	}
 }
